450b5b436aa00c8e3997eba9a3019e1c611872f4
ARochniak <15bblinn@gmail.com>
2019-11-29 14:56:31 +0200

    Updates

---
+++ precache-manifest.dcece2c4fbe3c4dcb5c552c0a8b093eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e24a800c96347bba772b4f317d7ed8ac",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "05719cdfc8b3d78ddb44",
+    "url": "/weatherapp/static/css/main.cc96df69.chunk.css"
+  },
+  {
+    "revision": "c94acd136fefb7f56d04",
+    "url": "/weatherapp/static/js/2.af22f2c5.chunk.js"
+  },
+  {
+    "revision": "05719cdfc8b3d78ddb44",
+    "url": "/weatherapp/static/js/main.e7cde001.chunk.js"
+  },
+  {
+    "revision": "cd586449e117cbbf3346",
+    "url": "/weatherapp/static/js/runtime-main.d198a0ad.js"
+  }
+]);
